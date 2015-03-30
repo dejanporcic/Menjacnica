91849d71abdf83eda02c3edfe5d75a7b34771e89
dejanporcic <dejan.626@gmail.com>
2015-03-31 01:05:54 +0200

    Napravljena (nefunkcionalna) klasa Menjacnica u okviru paketa menjacnica koja implementira sve metode interfejsa MenjacnicaInterfejs.

---
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -0,0 +1,18 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import menjacnica.interfejsi.MenjacnicaInterfejs;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	public void dodajKurs (Kurs kurs) {
+	}
+	
+	public void obrisiKurs (Kurs kurs){
+		
+	}
+	public Kurs vratiKurs (String skraceniNaziv, GregorianCalendar datum) {
+		return null;
+	}
+}
