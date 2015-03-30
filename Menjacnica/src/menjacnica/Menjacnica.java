03eb427ba6aa9ce246886ea413d4e7cfe769c9b9
dejanporcic <dejan.626@gmail.com>
2015-03-31 01:44:40 +0200

    Dodato telo metode dodajKurs u klasi Menjacnica.

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -7,6 +7,9 @@ import menjacnica.interfejsi.MenjacnicaInterfejs;
 public class Menjacnica implements MenjacnicaInterfejs {
 
 	public void dodajKurs (Kurs kurs) {
+		if (kurs == null)
+			throw new RuntimeException("Kurs ne sme imati null vrednost");
+		
 	}
 	
 	public void obrisiKurs (Kurs kurs){
